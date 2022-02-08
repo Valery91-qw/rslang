499e9921e5278b2b95cc96975c0521ed63886482
Valery91-qw <vakriv91@gmail.com>
2022-02-08 21:20:22 +0300

    feat: create header component, connect app to react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,15 @@ import React from 'react';
 import './normalize.css';
 import './App.css';
 import Footer from './components/UI/pages/footer';
-import MyButton from './components/UI/buttons/MyButton';
-import Cards from './components/UI/Cards';
-// import funcFetchUser from './components/requests/funcFetchUser';
+import Header from './components/UI/pages/header/Header';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <div className="wrapper">
+        <Header />
         <div className="main">
           <h1>Hello World</h1>
-          <MyButton>Войти</MyButton>
-          <Cards width='100px' height='100px'>
-            <button>Кнопка</button>
-          </Cards>
         </div>
         <Footer />
       </div>
