e837fd848bd81a2d6f4f76c5e3f8ce40515bb190
igor2000xp <igorring@gmail.com>
2022-02-23 04:55:44 +0300

    feat: add user's dictionary page  2022.02.21 4

--- src/components/UI/pages/MainPage.tsx
+++ src/components/UI/pages/MainPage.tsx
@@ -1,21 +1,57 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import './css/MainPage.css';
 import wordAPI from '../../../dal/wordAPI/wordAPI';
 import usersAPI from '../../../dal/wordAPI/usersAPI';
+import FrameAboutApp from './MainPageFrames/FrameAboutApp';
+import ArrowRight from './MainPageFrames/ArrowRight';
+import FrameValera from './MainPageFrames/FrameValera';
+import FrameIgor from './MainPageFrames/FrameIgor';
 
 const MainPage:React.FC = () => {
+
+  // let countFrames = 0;
+  const [countFrames, setCountFrames] = useState<number>(0);
+  const [pageFrame, setPageFrame] = useState<JSX.Element>(<FrameAboutApp />);
+
   useEffect(() => {
     wordAPI.getWords();
     usersAPI.singIn();
   }, []);
 
+  const changeFrame = () => {
+    setCountFrames(countFrames + 1);
+  };
+  // let pageFrame:JSX.Element;
+  useEffect(() => {
+    if (countFrames > 2) setCountFrames(0);
+    console.log(`change Frame ${countFrames}`);
+    switch(countFrames) {
+      case 0:
+        setPageFrame(<FrameAboutApp />);
+        break;
+      case 1:
+        setPageFrame(<FrameValera />);
+        break;
+      case 2:
+        setPageFrame(<FrameIgor />);
+        break
+      default:
+        break;
+    }
+  },[countFrames])
+
   return (
     <div className="wrapper">
       <Header />
-      <div className="main">
-        <h1>Hello World, Мир, удивись!</h1>
+      <div className="main-wrapper">
+        <div className="main">
+          {pageFrame}
+          <button onClick={changeFrame} className='arrow-button'>
+            <ArrowRight />
+          </button>
+        </div>
       </div>
       <Footer />
     </div>
---
+++ src/components/UI/pages/MainPageFrames/FrameIgor.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './FrameAboutApp.module.css';
+import imgIgor from '../games/assets/igor.jpg';
+
+const FrameIgor:React.FC = () => {
+  return (
+    <div className={classes.AppFrame}>
+      <h1>Разработчики: Игорь</h1>
+      <img src={imgIgor} className='img-valera'/>
+      <h6>
+        Молодой, начинайщий программист из клуба "молодых пенсионеров".
+        Неглядя на свой возраст, все ещё подает надежды.
+      </h6>
+      <p>
+        Сверстал главную страницу, разработал и сверстал Учебник, Словарь и страничку Статистики.
+        Освоил в части касающейся разработки данного проекта backend, React, Redux, Router, axios.
+        Вел непримиримую борьбу с eslint.
+      </p>
+    </div>
+  );
+};
+
+export default FrameIgor;
