47a9211dfeab5e0e60d0b05fa204b20d4af0ea62
Valery91-qw <vakriv91@gmail.com>
2022-03-20 18:22:58 +0300

    feat: create complexWords reducer

---
+++ src/bll/complex-words/complexWords.constants.ts
@@ -0,0 +1,7 @@
+enum ComplexWordsConstants {
+    add_word = 'complexWord/addWord',
+    delete_word = 'complexWord/deleteWord',
+    increase_show_number = 'complexWord/increaseShowNumber',
+}
+
+export default ComplexWordsConstants
---
+++ src/bll/complex-words/complexWordsReducer.ts
@@ -0,0 +1,18 @@
+import {ComplexWordsState, IComplexWordsState} from "./complexWordsState";
+import ComplexWordsConstants from "./complexWords.constants";
+import {ComplexWordsActionsType} from "./complexWordsActions";
+
+export default function complexWordsReducer(state = ComplexWordsState, action: ComplexWordsActionsType): IComplexWordsState {
+    switch (action.type) {
+        case ComplexWordsConstants.add_word:
+            return {...state, complexWords: [...state.complexWords, action.word]};
+        case ComplexWordsConstants.delete_word:
+            return {...state, complexWords: state.complexWords.filter(word => word.id !== action.id)}
+        case ComplexWordsConstants.increase_show_number:
+            return {...state, complexWords: state.complexWords.map(
+                    word => word.id === action.id ? {...word, showNumber: word.showNumber++} : word
+                )}
+        default:
+            return state;
+    }
+}
