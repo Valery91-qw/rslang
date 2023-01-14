19f470a192ce342cc98d5f7533199cf991de796d
Valery91-qw <vakriv91@gmail.com>
2023-01-14 12:34:48 +0300

    fix: change link to back

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 // 'https://react-learnwords-example.herokuapp.com';
 import { IWord } from './types/types';
 
-export const API_URL = 'https://rslang03.herokuapp.com';
+export const API_URL = 'https://rslang-prod-rslang-eia30x.mo2.mogenius.io/';
 export const FILES_URL = 'https://raw.githubusercontent.com/igor2000xp/rslang-asset/main/';
 // export const IMG_URL = 'https://raw.githubusercontent.com/igor2000xp/rslang-asset/main/img/';
 export const SVG_URL = 'https://raw.githubusercontent.com/igor2000xp/rslang-asset/main/img/main/svg/';
